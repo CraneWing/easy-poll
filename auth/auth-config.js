9b7514f446c7e35f36a87b2e49f8bfb19aeabdf5
Victoria Mielke <victoria.mielke@yahoo.com>
2017-02-17 19:33:27 +0000

    moved twitter auth to Heroku

--- auth/auth-config.js
+++ auth/auth-config.js
@@ -2,7 +2,7 @@ var ids = {
 	twitter: {
 		consumerKey: 'nWJss0e3z6xC4F5itqhv3x1r9',
 		consumerSecret: '3HPROCmDhyLmzXkTGqaVgN87mV1gcSmwQhkM3MXV5iwbDGGyvy',
-		calbackURL: 'https://easy-poll-cranewing.c9users.io/users/auth/twitter/callback'
+		calbackURL: 'https://ez-poll.herokuapp.com/users/auth/twitter/callback'
 	}
 };
 
